397acc7a75007070c2d98d79b936c331e2c9946e
bacely <bacelyy@gmail.com>
2023-07-26 10:22:37 +0200

    First commit

--- README.md
+++ README.md
@@ -1,11 +1,7 @@
 # 📦 Dashboard (`version 2`)
 
-This is a dashboard app created in Python using Streamlit.
+This is a dashboard app created in Python to predict cyber threat.
 
 ## Demo App
 
-[![Streamlit App](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://dash-board.streamlitapp.com/)
-
-## YouTube Tutorial
-
-<a href="https://youtu.be/o6wQ8zAkLxc"><img src="https://img.youtube.com/vi/o6wQ8zAkLxc/maxresdefault.jpg" alt="Building a Dashboard web app in Python - Full Streamlit Tutorial" title="Building a Dashboard web app in Python - Full Streamlit Tutorial" width="450"/></a>
+[![Schooly App](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://dash-board.streamlitapp.com/)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,63 +1,62 @@
 import streamlit as st
 import pandas as pd
-import plost
+import plotly.express as px
 
 st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 
-with open('style.css') as f:
-    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-    
-st.sidebar.header('Dashboard `version 2`')
-
-st.sidebar.subheader('Heat map parameter')
+st.sidebar.header('Cyberawareness Prediction App')
+st.sidebar.subheader('Heatmap Parameter')
 time_hist_color = st.sidebar.selectbox('Color by', ('temp_min', 'temp_max')) 
 
-st.sidebar.subheader('Donut chart parameter')
-donut_theta = st.sidebar.selectbox('Select data', ('q2', 'q3'))
+st.sidebar.subheader('Donut Chart Parameter')
+donut_theta = st.sidebar.selectbox('Select data', ('s1', 's2'))
 
-st.sidebar.subheader('Line chart parameters')
+st.sidebar.subheader('Line Chart Parameters')
 plot_data = st.sidebar.multiselect('Select data', ['temp_min', 'temp_max'], ['temp_min', 'temp_max'])
 plot_height = st.sidebar.slider('Specify plot height', 200, 500, 250)
 
 st.sidebar.markdown('''
 ---
-Created with ❤️ by [Data Professor](https://youtube.com/dataprofessor/).
+Created with ❤️ by [Schooly](https://www.theschooly.co/).
 ''')
-
-
+                    
 # Row A
-st.markdown('### Metrics')
+
+st.markdown('### Cyberawareness Metrics')
 col1, col2, col3 = st.columns(3)
-col1.metric("Temperature", "70 °F", "1.2 °F")
-col2.metric("Wind", "9 mph", "-8%")
-col3.metric("Humidity", "86%", "4%")
+col1.metric("Phishing Attempts", "22", "+10%")
+col2.metric("Security Training", "85%", "+5%")
+col3.metric("Password Strength", "Strong", "+30")
 
 # Row B
 seattle_weather = pd.read_csv('https://raw.githubusercontent.com/tvst/plost/master/data/seattle-weather.csv', parse_dates=['date'])
 stocks = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/stocks_toy.csv')
 
-c1, c2 = st.columns((7,3))
+c1, c2 = st.columns((7, 3))
 with c1:
     st.markdown('### Heatmap')
-    plost.time_hist(
-    data=seattle_weather,
-    date='date',
-    x_unit='week',
-    y_unit='day',
-    color=time_hist_color,
-    aggregate='median',
-    legend=None,
-    height=345,
-    use_container_width=True)
+    fig = px.histogram(
+        data_frame=seattle_weather,
+        x='date',
+        color=time_hist_color,
+        histfunc='median',
+        height=345)
+    st.plotly_chart(fig, use_container_width=True)
+
 with c2:
-    st.markdown('### Donut chart')
-    plost.donut_chart(
-        data=stocks,
-        theta=donut_theta,
-        color='company',
-        legend='bottom', 
-        use_container_width=True)
+    st.markdown('### Donut Chart')
+    fig = px.pie(
+        data_frame=stocks,
+        names='company',
+        values=donut_theta,
+        hole=0.5)
+    st.plotly_chart(fig, use_container_width=True)
 
 # Row C
-st.markdown('### Line chart')
-st.line_chart(seattle_weather, x = 'date', y = plot_data, height = plot_height)
+st.markdown('### Line Chart')
+fig = px.line(
+    data_frame=seattle_weather,
+    x='date',
+    y=plot_data,
+    height=plot_height)
+st.plotly_chart(fig)
